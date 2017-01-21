cb06b56dae68f4cf4ea138c32b760f418f492f0e
suhassatish <suhas.satish@gmail.com>
2017-01-20 21:25:12 -0800

    palindromic substrings

---
+++ app/palindromic_substrings.py
@@ -0,0 +1,33 @@
+"""
+Given a string, find all palindromes among its substrings
+eg - input: "abcacbbbca"
+output = {"bb", "cac", "bcacb", "bbb", "cbbbc", "acbbbca"}
+"""
+
+
+def palindromic_substrings(str1):
+    palset = set()
+    for i in xrange(len(str1)):
+        add_palindrome(str1, palset, i, i)  # finds odd-length palindromes
+        add_palindrome(str1, palset, i, i + 1)  # finds even-length palindromes
+    return palset
+
+
+def add_palindrome(str1, palindrome_set, begin_index, end_index):
+    """
+    Note on time complexity: for sparse palindrome strings, it is O(n) since the while loop
+    quickly terminates in just 1 iteration
+
+    For palindrome-dense strings, it is O(n^2) as potentiall at every position, you can expand
+    outwards to the whole string and keep discovering palindromes
+    :param str1:
+    :param palindrome_set:
+    :param begin_index:
+    :param end_index:
+    :return:
+    """
+    while begin_index >= 0 and end_index < len(str1) and str1[begin_index] == str1[end_index]:
+        if end_index - begin_index > 0:
+            palindrome_set.add(str1[begin_index:end_index+1])
+        begin_index -= 1
+        end_index += 1
