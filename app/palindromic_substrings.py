26fe873eb7b518368308f3ec4348943243ced447
suhassatish <suhas.satish@gmail.com>
2017-01-20 22:09:57 -0800

    laindromic_substrings: linear time solution link

--- app/palindromic_substrings.py
+++ app/palindromic_substrings.py
@@ -2,6 +2,16 @@
 Given a string, find all palindromes among its substrings
 eg - input: "abcacbbbca"
 output = {"bb", "cac", "bcacb", "bbb", "cbbbc", "acbbbca"}
+
+The brute force solution would be to find
+a) all substrings of the string (you can choose a substring by choosing 2 indices i,j such that
+substr = str[i:j+1]. Total ways to pick i,j such that 0 <= i,j <= n is nC2 = n!/(2! * (n-2)!) )
+= n * (n -1) / 2 = O(n^2)
+and for each substring,
+b) consider if its a palindrome. (This can be done in 1-pass of 2 pointers i & j
+traversing substring from opposite ends until they meet in the middle i.e., in O(n) time)
+
+so its O(n^3)
 """
 
 
@@ -31,3 +41,7 @@ def add_palindrome(str1, palindrome_set, begin_index, end_index):
             palindrome_set.add(str1[begin_index:end_index+1])
         begin_index -= 1
         end_index += 1
+
+# There is also a more complex O(n) algorithm, but getting it and implementing it in an interview
+#  will probable take longer than 45 minutes
+# https://www.akalin.com/longest-palindrome-linear-time
