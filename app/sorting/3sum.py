41843111f39cbd7369a33a6aa1e16fe91776706f
suhassatish <suhas.satish@gmail.com>
2017-02-18 21:07:55 -0800

    find all triplets that sum to 0 in an array

---
+++ app/sorting/3sum.py
@@ -0,0 +1,26 @@
+"""
+Find all triplets in an integer array that sum to 0
+"""
+
+
+def sum3(arr):
+    if arr is None or len(arr) < 3:
+        return None
+    arr.sort()
+    s = set()
+    for k in xrange(len(arr) - 1):
+        i = k + 1
+        j = len(arr) - 1
+        while i < j:
+            if arr[i] + arr[j] < -arr[k]:
+                i += 1
+            elif arr[i] + arr[j] > -arr[k]:
+                j -= 1
+            else:
+                s.add((arr[i], arr[j], arr[k]))
+                i += 1
+    return s
+
+
+if __name__ == '__main__':
+    print sum3([-1, -2, 0, 1, 3])
