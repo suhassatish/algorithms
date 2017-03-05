bc3db3831390943c1012c889630bb306f772a520
suhassatish <suhas.satish@gmail.com>
2017-03-05 10:22:29 -0800

    parenthesis matching. sub-array sum to target - boolean

---
+++ app/recursion/parens_print_matching.py
@@ -0,0 +1,39 @@
+"""
+Given N, print all valid pairs of parentheses with N pairs.
+eg - brackets(1) = ()
+brackets(2) = ()(), (()))
+brackets(3) = ((())), ()()(), (()))(), ()(()), (()())
+
+We observe that
+
+1) the remaining_left (after inserting some) should always be <= right_remaining
+eg - this would avoid scenario like '))'
+
+2) The termination case is when we have put down all k pairs in the prefix such that
+ left_remaining = right_remaining = 0
+"""
+
+
+def brackets(n):
+    st = set()
+    _brackets(n, n, '', st)
+    print st
+
+
+def _brackets(left_remaining, right_remaining, prefix, valid_set):
+    if left_remaining == 0 and right_remaining == 0:
+        valid_set.add(prefix)
+        return
+
+    if left_remaining <= right_remaining and left_remaining > 0:
+        #  can still afford to insert '('
+        _brackets(left_remaining - 1, right_remaining, prefix + '(', valid_set)
+
+    if left_remaining <= right_remaining and right_remaining > 0:
+        #  can still afford to insert ')'
+        _brackets(left_remaining, right_remaining - 1, prefix + ')', valid_set)
+
+
+if __name__ == '__main__':
+    for i in xrange(1):
+        brackets(i)
---
+++ app/recursion/subset_sum.py
@@ -0,0 +1,75 @@
+"""
+Given an array of integers and a target integer K, find if there are a group of integers,
+not necessarily contiguous, that can sum to K.
+
+eg - [2,4,8], 6 -> True since 2+4 = 6
+[2,-4,8], 1 -> False
+[2,4,8], 14 -> True since 2+4+8 = 14
+[2,4,8], 9 -> False
+
+Brute force approach 1:
+We can choose just 1 number that sums to K, (can choose in N ways in an N-element array)
+or 2 numbers that sum to K, can choose in NC2 ways
+...
+or n - 1 numbers that sum to K, can choose in N C (N-1) ways
+
+So in brute force solution, there are NC1 + NC2 + NC3 + ... NC (N-1) ways
+
+Brute force approach 2:
+Find all subsets of a set. There are 2^k of them, then filter the subsets that sum to K.
+
+Back tracking optimization: If a subset_sum has already exceeded target, then discard that subset
+and all containing those. Tree pruning.
+
+http://www.geeksforgeeks.org/dynamic-programming-subset-sum-problem/
+Partitioning a set of integers into K non-zero partitions to evaluate to a target is a well-known
+NP-complete problem. There are no known polynomial-time solutions. Best you can do is use DP
+to get a pseudo-polynomial time solution.
+
+TODO:
+1) Code the dynamic programing solution.
+http://www.geeksforgeeks.org/find-subarray-with-given-sum/
+
+2) Return the integers that sum to the target - iterative solution here
+http://stackoverflow.com/questions/18305843/find-all-subsets-that-sum-to-a-particular-value
+"""
+
+
+def is_subset_sum(input_list, target):
+    return _is_subset_sum(input_list, len(input_list), target)
+
+
+def _is_subset_sum(input_list, num_elements_remaining, remaining_target):
+    # import ipdb; ipdb.set_trace()
+    if remaining_target == 0:
+        return True
+    if num_elements_remaining == 0 and remaining_target != 0:
+        return False
+
+    # early exit optimization
+    # if last array element is greater than sum then ignore it
+    # if input_list[num_elements_remaining - 1] > sum:
+    #    num_elements_remaining -= 1
+
+    # check if sum can be obtained by either including OR excluding the last element
+    last_element = input_list[num_elements_remaining - 1]
+    return (_is_subset_sum(
+        input_list,
+        num_elements_remaining - 1,
+        remaining_target - last_element
+        # numbers_so_far.add(last_element)
+    ) or
+            _is_subset_sum(
+        input_list,
+        num_elements_remaining - 1,
+        remaining_target
+        # numbers_so_far
+    )
+            )
+
+
+if __name__ == '__main__':
+    print is_subset_sum([2,4,8], 6)   # True since 2+4 = 6
+    print is_subset_sum([2,-4,8], 1)  # False
+    print is_subset_sum([2,4,8], 14)  # True since 2+4+8 = 14
+    print is_subset_sum([2,4,8], 9)  # False
