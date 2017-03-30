c98e0d696d5c670db77dd249f32f6db3b7e6a592
suhassatish <suhas.satish@gmail.com>
2017-03-30 15:24:01 -0700

    send grid ph screen

---
+++ app/interviews/send_grid.py
@@ -0,0 +1,44 @@
+"""
+stack vs queue
+
+different implementations stack vs queue
+
+algorithm - what would you use to implement recursion and why?
+
+implicit call stack vs queue;
+
+breadth-first traversal is for queue;
+
+hashing time complexity - O(1) look-up;
+
+
+concurrency vs parallelism;
+concurrency is usually on a single machine, as multi-threading or a multi-processing;
+
+race condition - when does it occur? double-checked locking or synchronization.
+
+example of time you learnt ; ruby twitter project ;
+
+data to make decision for yourself;
+
+either failed a commitment or expectations;
+
+slips - what happened in those instances?
+
+on-board  a team member or a mentor?
+on-boarding overall?
+
+characteristics of good operability of software; services in live environment -
+logging; resource intensive => should free up and clean-up after itself;
+
+what sort of things help ask someone to troubleshoot production issues?
+readable api, testable, modular
+
+challenges in distributed computing env - stack 100s of 1ks of servers in near-real time,
+ops at scale
+
+node; durability; consistency; high-availability;
+
+performance issues?
+
+"""
