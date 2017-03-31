69133bedc89b598a44b4edd292e2439fb27c3fd4
suhassatish <suhas.satish@gmail.com>
2017-03-31 08:28:31 -0700

    graph_api with depth-first-search

---
+++ app/graphs/dfs_bfs_topological_sort.py
@@ -0,0 +1,150 @@
+"""
+DEPTH-FIRST SEARCH PROPERTIES
+
+DFS runs in O(E + V) time.
+
+PROPOSITION: DFS marks all vertices connected to s in time proportional to
+the sum of their degrees.
+
+PROPOSITION: After DFS, can find vertices connected to s in constant time
+and can find a path to s (if one exists) in time proportional to its length.
+
+QUESTION: In a Graph G represented using the adjacency-lists representation,
+depth-first search marks all vertices connected to s in time proportional to
+ANSWER: The sum of the degrees of the vertices in the connected component
+containing s
+
+QUESTION: The critical data structures used in depth-first search and
+breadth-first search are _____ and ____, respectively.
+ANSWER: DFS->Stack BFS->Queue
+EXPLANATION: With DFS, it is either an explicit stack(w/ nonrecursive version)
+or the function-call stack (w/a recursive version)
+"""
+from graph_api import Graph
+
+
+class DepthFirstPaths(object):
+
+    def __init__(self, Grph, src):
+        self.src = src
+        self.edge_to = [None] * Grph.v()  # edge_to[v] = last edge on s-v path
+        self.marked = [False] * Grph.v()  # marked[v] = is there an s-v path?
+        self.dfs(Grph, src)
+
+    def dfs(self, Grph, v):
+        """
+        Runs in O(V+E) time
+        depth first search from vertex v
+        :param Grph:
+        :param v:
+        :return:
+        """
+        self.marked[v] = True
+        for w in Grph.adj(v):
+            if not self.marked[w]:
+                # import ipdb; ipdb.set_trace()
+                self.edge_to[w] = v
+                self.dfs(Grph, w)
+
+    def has_path_to(self, v):
+        """
+        is there a path between source vertex s and vertex v?
+        :param v:
+        :return:
+        """
+        return self.marked[v]
+
+    def path_to(self, v):
+        """
+        returns a path between source vertex s and vertex v, or None if it doesnt exist
+        :param v:
+        :return:
+        """
+        if not self.has_path_to(v):
+            return None
+
+        path_stk = []  # stack for path
+        # walk from v to src adding the edges into the stack
+        x = v
+        while x != self.src:
+            path_stk.append(x)  # push
+            x = self.edge_to[x]
+        path_stk.append(self.src)
+        return path_stk
+
+
+"""
+def bfs(v):
+
+    Time complexity is O(V + E)
+    Biggest application of breadth-first-search is Dijkstra's shortest path
+    Shortest path is the theme of 70% of graph interviews
+    :param v:
+    :return:
+
+    visited = set()
+    q = Queue()
+    q.put(v)
+
+    while not q.empty():
+        vertex = q.get()
+
+        if vertex in visited:
+            continue
+
+        print vertex.val
+        visited.add(vertex)
+        for n in vertex.neighbors:
+            q.put(n)
+
+
+def shortest_path(start, end):
+
+    Keep track of who enqueue what in back_refs dictionary.
+
+    Dijkstra works with cycles and works for directed graphs.
+    It doesn't work only for negative-weighted edges
+    :param start:
+    :param end:
+    :return:
+
+    back_refs = {start: None}
+    q = Queue()
+    q.put(start)
+    while not q.empty():
+        v = q.get()
+        if v == end:
+            break
+
+        for n in v.neighbors:
+            if n not in back_refs:
+                back_refs[n] = v
+                q.put(n)
+
+    # if end is not found
+    if end not in back_refs:
+        return None
+    pass
+    # TODO: there's more here, refer video lecture
+
+
+def test_bfs():
+
+    6 <- 2 -> 7
+         ^
+         |    /->8
+         1-> 3
+             \->9
+    :return:
+
+"""
+
+if __name__ == '__main__':
+    l = [13, 13, (0, 5), (4,3), (0,1), (9, 12), (6,4), (5,4), (0,2), (11, 12), (9, 10), (0,6),
+         (7,8), (9,11), (5,3)]
+    g = Graph(l)
+    dfp = DepthFirstPaths(g, 6)
+    print dfp.edge_to  # [6, 0, 0, 5, 3, 0, None, None, None, None, None, None, None]
+    print dfp.marked  # [True, True, True, True, True, True, True, False, False, False, False,
+    # False, False]
+    print dfp.path_to(4)  # [4, 3, 5, 0, 6]  Note: not necessarily the shortest path
---
+++ app/graphs/graph_api.py
@@ -0,0 +1,142 @@
+"""
+Vertex (V) and edge (E)
+Directed vs undirected graphs
+
+Cyclic vs acyclic graphs.
+
+eg - loop in a linked list = cyclic directed graph. Linked lists and trees are subsets of graphs.
+
+Degree of a vertex. In-degree = number of incoming edges ,
+ out-degree (default) = number of outgoing edges.
+
+Degree of a graph - Maximum degree of any node.
+
+Connected component of a vertex: All vertices that are reachable from current vertex.
+1-> 2-> 3-> 4->5->6
+^-----------------|
+
+
+Click in a graph is every vertex is connected to every other vertex (fully connected)
+
+Sink: Vertex that has in-degree, but not out-degree.
+
+Implementation: Usually as adjacency_matrix and adjacency_list
+
+Adjacency matrix:
+   0  1  2  3  4
+0        1     1
+1  1  1
+2           1  1
+3  1  1  1  1  1
+4
+
+Adjacency list:
+{
+    0: [2, 4],
+    1: [0, 1],
+    2: [3, 4],
+    3: [0, 1, 2, 3, 4],
+    4: []
+}
+
+Pros & Cons:
+adj_matrix can answer in constant time. TODO: revisit video lecture
+adj_list takes time = degree of graph
+
+
+adj_matrix = not good for sparse graphs
+adj_list = not good for dense graphs
+adj_set can be good for anything.
+
+Weighted vs unweighted graphs. Edges carry weights.
+
+Traversal: breadth-first search (bfs) and depth-first search (dfs)
+Anything you do recursively can be done iteratively using a stack.
+But the vice-versa is not true.
+
+Litmus tests for graph problems:
+Safest way is by doing bfs, will not have any stack overflows.
+Dfs is the shortest way to do it.
+
+There are 2 kinds of graph problems:
+1) Its obvious that its a graph problem. Explicit graph problems
+2) Implicit graph problems
+
+Clone entire connected component of a given vertex. Return the clone. Dont forget to clone the
+edges as well. Have a hash map of original to clone. This can serve as the visited set. Can do it
+using both bfs or dfs.
+
+"""
+from Queue import Queue
+
+
+class Graph(object):
+    def __init__(self, a):
+        if isinstance(a, int):
+            self._init_empty(a)
+        elif isinstance(a, Graph):
+            self._init_from_graph(a)
+        elif isinstance(a, list) and len(a) == 1:
+            self._init_empty(a[0])
+        elif isinstance(a, list):
+            self._init(a)
+
+    def _init_empty(self, num_vertices):
+        if num_vertices < 0:
+            raise Exception("Number of vertices must be non-negative")
+        self._adj = [None] * num_vertices  # this is an adjacency_list indexed by vertex
+        self._V = num_vertices
+        self._E = 0
+
+        # each indexed element is a set of nodes that are connected to this vertex
+        for v in xrange(num_vertices):
+            self._adj[v] = set()
+
+    def _init(self, input_stream_with_edges):
+        """
+        The format of input_stream_with_edges is a list.
+        The number of vertices is lst[0], lst[1] = number_of_edges , lst[2:] = tuple of edges
+        :param input_stream_with_edges:
+        :return:
+        """
+        self._init_empty(input_stream_with_edges[0])
+        self._E = input_stream_with_edges[1]
+        if self._E < 0:
+            raise Exception("Number of edges must be non-negative")
+        for v,w in input_stream_with_edges[2:]:
+            self.add_edge(v, w)
+
+    def _init_from_graph(self, graph_obj):
+        """
+        This deep copies the input graph_obj to create a cloned graph
+        :return:
+        """
+        self._init_empty(graph_obj.v())
+        self._E = graph_obj.e()
+        for v in xrange(graph_obj.v()):
+            for w in graph_obj.adj(v):
+                self._adj[v].add(w)
+
+    def add_edge(self, v, w):
+        """
+        Adds the undirected edge v-w to self graph
+        :param v:
+        :param w:
+        :return:
+        """
+        self._adj[v].add(w)
+        self._adj[w].add(v)
+
+    def v(self):
+        return self._V
+
+    def e(self):
+        return self._E
+
+    def adj(self, v):
+        """
+        returns an iterable set of vertices that are adjacent to vertex v
+        :param v:
+        :return:
+        """
+        return self._adj[v]
