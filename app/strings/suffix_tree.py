3934e049a062d1349c0da39b0e5411f9d2ffd03c
suhassatish <suhas.satish@gmail.com>
2017-04-05 09:11:33 -0700

    WIP: suffix_tree

---
+++ app/strings/suffix_tree.py
@@ -0,0 +1,11 @@
+"""
+ukkonen's algorithm is the only algorithm that constructs suffix trees in O(N)
+time.
+
+How to solve the longest_repeating_substring? most_repeating_substring is another
+standard question.
+
+Substring search is linear using rabin-karp or KMP or suffix-trees.
+Generate all substrings and for each substring, identify if it meets certain criteria.
+
+"""
