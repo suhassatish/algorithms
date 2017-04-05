17ae47a81e7f5526a65304e6fdb305d81efc95d0
suhassatish <suhas.satish@gmail.com>
2017-04-05 09:08:50 -0700

    moving_window top 5 in any 30 day window

---
+++ app/arrays/timestamp_epoch_windows.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+# timestamps of logins in ascending order
+timestamps = [
+    1416182478,  #EPOCH TIME
+    1416182490,
+    # ...
+]
+
+
+def has_five_or_more_in_ANY_30_days(timestamps):
+    # should return True or False
+    for i,time in enumerate(timestamps):
+        start_epoch = time
+        end_epoch = epoch(start_epoch + '30days')
+        count = 1
+        for j in xrange(i+ 1, len(timestamps)):
+            if timestamps[j] > end_epoch:
+                break
+            else:
+                count += 1
+            if count == 5:
+                return True
+    return False
+
+
+def epoch(time_range):
+    pass
