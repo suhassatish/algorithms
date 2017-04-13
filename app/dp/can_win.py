5f92efdccb335da4dc32976dc0e65d51ffe5553b
suhassatish <suhas.satish@gmail.com>
2017-04-13 12:06:57 -0700

    can_win game - race to 0

---
+++ app/dp/can_win.py
@@ -0,0 +1,57 @@
+# canWin
+# board = [0, 3, 1, 2, 3, 0, 10]
+# goal: move to value 0 cell.
+# if value > 0, you can jump left or right by (value) steps
+# if there exists a path ---> value 0 cell, we can win
+# boolean "canWin" value whether there exists such a path
+# given a board, startIndex, return the canWin
+
+
+def can_win(a, start_index):
+    visited = [False] * len(a)
+    return _can_win(a, start_index, visited)
+
+
+def _can_win(a, index, visited):
+    print "index = %s" % index
+
+    if a[index] == 0:
+        return True
+
+    left_ret_value = right_ret_value = False
+
+    left_index = index - a[index]
+    right_index = index + a[index]
+
+    if valid(a, left_index) and not visited[left_index]:
+        visited[left_index] = True
+        left_ret_value = _can_win(a, left_index, visited)
+
+    if valid(a, right_index) and not visited[right_index]:
+        visited[right_index] = True
+        right_ret_value = _can_win(a, right_index, visited)
+
+    if left_ret_value or right_ret_value:
+        return True
+
+    return False
+
+
+def valid(a, index):
+    if 0 <= index < len(a):
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    #board = [0, 3, 1, 2, 3, 0, 10]
+    #print can_win(board, 3)  # True
+
+    #board = [1, 3, 1, 2, 3, 1, 10]
+    #print can_win(board, 3)
+
+    #board = [1, 3, 1, 2, 3, 1, 0]
+    #print can_win(board, 3)
+
+    board = [10, 0, 1, 2, 3, 1, 10]
+    print can_win(board, 3)
