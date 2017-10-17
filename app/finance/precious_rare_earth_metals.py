3e0f3f4c6444ab8118e949464ccbb878e009b3ea
suhassatish <suhas.satish@gmail.com>
2017-10-17 08:47:40 -0700

    Finance & investing 101: real estate, IRA, precious metals

---
+++ app/finance/precious_rare_earth_metals.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Used in Li-ion batteries for electric cars (% of USA requirement thats imported)
+    1) Lithium (> 50%)
+    2) Cobalt (74%)
+    3) Manganese (100%)
+-------------------
+Used in Solar Technologies
+    1) Indium (100% of requirement by US is imported)
+    2) Tellurium (> 75%)
+    3) Gallium (100%)
+
+Used in high performance magnets
+    1) Rare Earth metals (100%)
+
+-------------------
+Technology mineral prices like Cobalt prices went up 76% in 2017 and Lithium went up 21%.
+
+Main countries producing following metals:
+Source: Fortune.com July 01, 2017
+US Geological Survey, Energy & Environmental policy, Univ of Delaware
+Prof Saleem Ali, energy & environment - Univ. Delaware - published paper warning shortfalls in supply of metals.
+
+LITHIUM - Chile, Argentina, China
+
+COBALT - Norway, Finland, China, Japan
+
+MANGANESE - south africa, gabon, georgia, Australia
+
+RARE EARTH - France, Estonia, China, Japan
+
+INDIUM - Canada, France, Belgium, China
+
+TELLURIUM - Canada, Belgium, China, Phillippines
+
+GALLIUM - UK, Germany, Ukraine, China
+-------------------
+Which precious metals are allowed in an IRA?
+
+Precious metals can be held in your IRA in the form of physical gold, silver, platinum, and palladium bullion products.
+The precious metals allowed in an IRA must meet their respective minimum fineness requirement.
+Gold – 99.5%
+Silver – 99.9%
+Platinum – 99.95%
+Palladium – 99.95%
+-------------------
+ENTRUST precious metals IRA fees -
+There are two fees associated with establishing a new Precious Metals IRA at Entrust.
+
+    1) Entrust charges an all-inclusive  flat rate $150 annual record keeping fee, with no set-up cost and no additional
+    transaction costs. You may accumulate metals over the years without any additional costs.
+
+    2) There is also an annual storage fee from the depository you choose. Depending on the depository, these fees range
+    from an average of $95 to $300.
+
+    2b) The metals are stored in an approved depository.  You may choose one of 5 depository companies (with 7
+    nation-wide locations) that are IRA approved, regulated, and insured facilities.
+-------------------
+
+"""
---
+++ app/finance/real_estate_investing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+SECONDARY LIEN LOAN, UNSECURED-DEBT, SUBORDINATED-DEBT
+
+1) SECONDARY LIEN LOAN - In case of bankruptcy, the primary lenders (primary lien) get the 1st repayment, and then the
+secondary lenders will get whatever is remaining. So its more risky investment.
+
+2) SECURED-DEBT - pledge of specific assets of the borrower (e.g., buildings, land, equipment, intellectual property,
+receivables and other financial assets). Secured holders can initiate foreclosure of property to recover their debt.
+Unsecured creditors dont have right to foreclosure and hence have less collateral backing their investment claim.
+
+3) SUBORDINATED-DEBT - Subordinated debt refers to a class of obligations that are contractually subordinated in ranking
+to all of the senior obligations (i.e., general non-subordinated obligations) of the company, whether they are secured
+or unsecured.
+------------------
+NNN - A triple net lease REIT usually invests in real estate where the tenant is responsible for paying the building's
+property taxes, insurance and costs associated with maintenance or repairs.
+
+Triple net leased properties are popular investment vehicles for investors seeking STEADY INCOME WITH RELATIVELY LOW
+RISK.
+------------------
+PREFERRED EQUITY -
+
+------------------
+CAPITALIZATION RATE (aka cap rate) - Experienced real estate investors use this metric as a preferred way to assess ROI
+on an opportunity.
+------------------
+PRIVATE EQUITY investment model fees - management fees of “2&20”—meaning 2% in an annual asset management fee and 20% of
+the profit share. VAST MAJORITY OF THESE FUNDS DO NOT BEAT THE MARKET.
+OVER THE LONG TERM, THE HIGHER THE FEES, THE LOWER THE NET PERFORMANCE.
+Minimum investment is usually $100K, like HEDGE FUNDS.
+
+Terms - approximately 10 years lock-in period. May have extension periods.
+------------------
+"""
