fff60a6c585b34841b82e21a7dd4d97624a8bdfa
Suhas <suhas.satish@gmail.com>
2018-06-25 12:48:02 -0700

    Functional programing and category theory concepts - terminologies to read

---
+++ app/functional_programing/func_prog.py
@@ -0,0 +1,31 @@
+"""
+Notes from talk "Absurdly vast applicability of monoids in javascript" -
+
+1) closed and associative and identity => monoids. 
+union of sets if a mnoid but not an intersection. intersection is a semi-group.
+
+
+  a) closed => chainable
+
+  b) asociative => eval order
+
+  c) identity => fail safe.
+---------------------
+concat - 
+
+"goodbye".concat(" cruel")
+
+any data structure built entirely from a monoid is a monoid.
+--------------
+TERMINOLOGIES TO READ - (from category theory) (TODO)
+1) monoid homomorphism
+2) Cayley's theorem
+
+3) applicative functors
+4) endofunctors
+
+5) algebraic groups
+6) semi-rings - (useful for speech recognition in state machines) 
+
+
+"""
